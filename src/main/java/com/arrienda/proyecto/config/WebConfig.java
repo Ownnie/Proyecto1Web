e5e0c6c09806063186c7245757b5d157e7f98542
juafra4 <juafra454@outlook.com>
2024-10-28 19:23:41 -0500

    Conexion con el front

--- src/main/java/com/arrienda/proyecto/config/WebConfig.java
+++ src/main/java/com/arrienda/proyecto/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/") // Permite todos los endpoints
+        registry.addMapping("/**") // Permite todos los endpoints
                 .allowedOrigins("http://localhost:4200") // Permite el origen del frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*")
